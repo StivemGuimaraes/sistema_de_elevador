44952038df21c3913b32ce1da13ea37a3350d7bb
João <Joo@JOAO>
2020-07-21 17:36:00 -0300

    Add project files.

---
+++ sistema de um elevador/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_de_um_elevador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
